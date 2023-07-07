f34061f90e59dcab8fe039c07756d2861e627a1a
Archimondee <kikchiemedia@gmail.com>
2023-07-08 01:29:08 +0700

    Fixing

---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "\nInitializing git repository\n"
+git init
+
+echo "\nInitializing husky (Please hit return/enter key for the next prompt)\n"
+npx husky-init
+
+echo "\nInstalling node_modules\n"
+npm install
+
+echo "\nInitializing husky hooks\n"
+npx husky set .husky/pre-commit "npm run test && npm run lint"
+
+# mv _env .env
+# mv _env.production .env.production
+# mv _env.staging .env.staging
