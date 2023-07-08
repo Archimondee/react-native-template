9fb585f60c30b094bd55b5ebc07c77633632d227
Archimondee <kikchiemedia@gmail.com>
2023-07-09 01:14:36 +0700

    Add eslint rule

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,3 +1,27 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+        alias: {
+          components: "./src/components",
+          screens: "./src/screens",
+          assets: "./src/assets",
+          navigations: "./src/navigations",
+          types: "./src/types",
+          utils: "./src/utils",
+          stores: "./src/stores",
+          hooks: "./src/hooks",
+          configs: "./src/configs",
+          managers: "./src/managers",
+          services: "./src/services",
+          atoms: "./src/atoms",
+        },
+      },
+    ],
+    //Reanimated plugin has to be listed last
+    "react-native-reanimated/plugin",
+  ],
 };
