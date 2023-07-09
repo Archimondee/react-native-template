a64ad6d5a0260af30e79053a7961051115eadce6
Archimondee <kikchiemedia@gmail.com>
2023-07-09 10:35:54 +0700

    Fixing installing node modules with yarn

--- script.sh
+++ script.sh
@@ -6,14 +6,14 @@ git init
 echo "\nInitializing husky (Please hit return/enter key for the next prompt)\n"
 npx husky-init
 
-echo "\nInstalling node_modules\n"
-npm install
+echo "\nInstalling node_modules with yarn\n"
+yarn install
 
 echo "\nInitializing husky hooks\n"
 npx husky set .husky/pre-commit "npm run lint"
 
 echo "\nInstalling Pods\n"
-cd template/ios && pod install && cd .. && cd ..
+cd ios && pod install && cd .. && cd ..
 
 # mv _env .env
 # mv _env.production .env.production
