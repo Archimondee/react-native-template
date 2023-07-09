e0d0dce2fd378b028b9cd14d0d3b689fe92fb703
Archimondee <kikchiemedia@gmail.com>
2023-07-10 02:01:27 +0700

    Remove env in script.sh

--- script.sh
+++ script.sh
@@ -14,8 +14,3 @@ npx husky set .husky/pre-commit "npm run lint"
 
 echo "\nInstalling Pods\n"
 cd ios && pod install && cd .. && cd ..
-
-mv _env .env
-mv _env.production .env.production
-mv _env.staging .env.staging
-mv _env.development .env.development
